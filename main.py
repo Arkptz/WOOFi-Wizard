71e88ef71f5829b2aa8c99c6132c701585dfeb22
Arkptz1 <58850875+Arkptz1@users.noreply.github.com>
2023-01-27 01:53:58 +0400

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class Wifoo(Flow):
             self.proxy.change_ip(only_check_inet=True)
         self.log_debug_with_lock(f'Старт потока {self.data}')
         self.restart_driver()
+        self.get_new(self.proxy.proxy_link)
         self.get_new('https://guild.xyz/woofi')
         self.wait_click('//button[@data-dd-action-name="Join"]')#join to guild roles
         self.wait_click('//button[@data-dd-action-name="Connect wallet (JoinModal)"]')# connect wallet
